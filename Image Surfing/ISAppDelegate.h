3f4ae2103841cc5da21ad9f337907a014e3536d1
Serge45 <serge45497@gmail.com>
2014-08-01 23:52:46 +0800

    Now support
    1. Read multiple images (*.jpg, *.png, *.bmp and *.tif),
    2. Move to next, previous, the first and the last image for loaded
    images,
    3. Current display image will be displayed in title bar.

--- Image Surfing/ISAppDelegate.h
+++ Image Surfing/ISAppDelegate.h
@@ -8,8 +8,21 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface ISAppDelegate : NSObject <NSApplicationDelegate>
+@interface ISAppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSImageView *imageView;
+@property (weak) IBOutlet NSScrollView *scrollView;
+@property (retain) NSImage *currentImage;
+@property (retain) NSMutableArray *imageList;
+@property (retain) NSMutableArray *imageNames;
+@property (nonatomic, assign) int curImageIdx;
+@property (retain) NSString *titlePrefix;
+@property (assign) BOOL showRealSize;
 
+- (IBAction)openFileClickedFor:(id)sender;
+- (IBAction)toNextImage:(id)sender;
+- (IBAction)toPreviousImage:(id)sender;
+- (IBAction)toFirstImage:(id)sender;
+- (IBAction)toLastImage:(id)sender;
 @end
--- Image Surfing/ISAppDelegate.m
+++ Image Surfing/ISAppDelegate.m
@@ -10,9 +10,98 @@
 
 @implementation ISAppDelegate
 
+@synthesize currentImage;
+@synthesize imageView;
+@synthesize scrollView;
+@synthesize imageList;
+@synthesize imageNames;
+@synthesize window;
+@synthesize titlePrefix;
+@synthesize showRealSize;
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    window.delegate = self;//resize signal processed by self
+    scrollView.documentView = imageView;
+    titlePrefix = @("Image Surfing");
+    _curImageIdx = -1;
+    showRealSize = NO;
+    [self updateImageViewSize];
+}
+
+- (IBAction)openFileClickedFor:(id)sender {
+    NSArray *allowedTypes = [[NSArray alloc] initWithObjects: @("jpg"),
+                                                              @("png"),
+                                                              @("bmp"),
+                                                              @("tiff"), nil];
+    
+    
+    NSOpenPanel *openPanel = [[NSOpenPanel alloc] init];
+    openPanel.allowsMultipleSelection = YES;
+    openPanel.canChooseDirectories = NO;
+    openPanel.canChooseFiles = YES;
+    openPanel.allowedFileTypes = allowedTypes;
+    NSInteger result = [openPanel runModal];
+    
+    if (result == NSFileHandlingPanelOKButton) {
+        if (!imageList) {
+            imageList = [[NSMutableArray alloc] init];
+        }
+        
+        if (!imageNames) {
+            imageNames = [[NSMutableArray alloc] init];
+        }
+        
+        [imageList removeAllObjects];
+        [imageNames removeAllObjects];
+        
+        for (NSURL *s in openPanel.URLs) {
+            NSString *path = s.path;
+            [imageList addObject: [[NSImage alloc] initWithContentsOfFile: path]];
+            [imageNames addObject: path];
+        }
+        
+        self.curImageIdx = 0;
+    }
+}
+
+- (IBAction)toNextImage:(id)sender {
+    self.curImageIdx = _curImageIdx + 1;
+}
+
+- (IBAction)toPreviousImage:(id)sender {
+    self.curImageIdx = _curImageIdx - 1;
+}
+
+- (void)setCurImageIdx:(int)idx {
+    if (idx >= 0 && idx < imageList.count) {
+        _curImageIdx = idx;
+        imageView.image = imageList[idx];
+        window.title = [[NSString alloc] initWithFormat: @("%@(%@)"), titlePrefix, imageNames[idx]];
+    }
+}
+
+- (IBAction)toFirstImage:(id)sender {
+    self.curImageIdx = 0;
+}
+
+- (IBAction)toLastImage:(id)sender {
+    self.curImageIdx = (int)imageList.count - 1;
+}
+
+- (void)updateImageViewSize {
+    if (showRealSize) {
+        
+    } else {
+        NSRect imageFrame = scrollView.frame;
+        imageFrame.size.width -= scrollView.verticalScroller.frame.size.width;
+        imageFrame.size.height -= scrollView.horizontalScroller.frame.size.height;
+        [imageView setFrame: imageFrame];
+    }
+}
+
+- (void)windowDidResize:(NSNotification *)notification {
+    [self updateImageViewSize];
 }
 
 @end
